8f4abc11c6e523ece25e698b9919de935d270b80
matviiko <matviy.kopchak@gmail.com>
2020-09-02 20:16:02 +0300

    module 1

--- src/App.js
+++ src/App.js
@@ -4,13 +4,27 @@ import Car from "./Car/Car";
 
 class App extends Component {
 
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            cars: [
+                {name: 'Ford', year: 2012},
+                {name: 'Mazda', year: 1991},
+                {name: 'Opel', year: 2014},
+            ]
+        }
+    }
+
   render() {
 
     return (
         <div className="App">
             <h1>{this.props.title}</h1>
 
-          <Car />
+          <Car name={this.state.cars[0].name} year={this.state.cars[0].year} />
+          <Car name={this.state.cars[1].name} year={this.state.cars[1].year} />
+          <Car name={this.state.cars[2].name} year={this.state.cars[2].year} />
         </div>
     );
   }
--- src/Car/Car.js
+++ src/Car/Car.js
@@ -1,12 +1,17 @@
 import React from "react";
 import './Car.css'
 
-export default (props) => {
-    return (
-        <div className="Car">
-            <h3>Car name: {props.name}</h3>
-            <p>Year :<strong>{props.year}</strong></p>
-            <button onClick={props.onChangeTitle}>Click</button>
-        </div>
-    )
+class Car extends React.Component {
+    render() {
+        return (
+
+            <div className="Car">
+                <h3>Car name: {this.props.name}</h3>
+                <p>Year :<strong>{this.props.year}</strong></p>
+                <button onClick={this.props.onChangeTitle}>Click</button>
+            </div>
+        )
+    }
 }
+
+export default Car
