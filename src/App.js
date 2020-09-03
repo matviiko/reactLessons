80ea4cd34f366a0d2f8c51144c1bbf25a6cf04ad
matviiko <matviy.kopchak@gmail.com>
2020-09-03 20:32:35 +0300

    add Quiz and ActiveQuiz

--- src/App.js
+++ src/App.js
@@ -1,34 +1,17 @@
 import React, {Component} from 'react';
-import './App.css';
-import Car from "./Car/Car";
+import Layout from "./hoc/Layout/Layout";
+import Quiz from "./containers/Quiz/Quiz";
 
 class App extends Component {
 
-    constructor(props) {
-        super(props)
-
-        this.state = {
-            cars: [
-                {name: 'Ford', year: 2012},
-                {name: 'Mazda', year: 1991},
-                {name: 'Opel', year: 2014},
-            ]
-        }
-    }
 
   render() {
-
     return (
-        <div className="App">
-            <h1>{this.props.title}</h1>
-
-          <Car name={this.state.cars[0].name} year={this.state.cars[0].year} />
-          <Car name={this.state.cars[1].name} year={this.state.cars[1].year} />
-          <Car name={this.state.cars[2].name} year={this.state.cars[2].year} />
-        </div>
+        <Layout>
+          <Quiz/>
+        </Layout>
     );
   }
-
 }
 
   export default App;
