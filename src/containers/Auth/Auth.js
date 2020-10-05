5a27150a594638cb17d4de5c3569f6a589728fca
matviiko <matviy.kopchak@gmail.com>
2020-10-06 00:23:44 +0300

    add REST for auth

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -3,6 +3,7 @@ import classes from './Auth.module.css'
 import Button from "../../components/UI/Button/Button";
 import Input from "../../components/UI/Input/Input";
 import is from "is_js"
+import axios from "axios"
 
 export default class Auth extends Component {
 
@@ -36,11 +37,35 @@ export default class Auth extends Component {
         }
     }
 
-    loginHandler = () => {
+    loginHandler = async () => {
+        const authData = {
+            email: this.state.formControls.email.value,
+            password: this.state.formControls.password.value,
+            returnSecureToken: true
+        }
 
+        try {
+            const response = await axios.post(`https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyAevsYkAm2KDeefKTe9Jw20l9tCypNiWIU`, authData)
+
+            console.log(response.data)
+        } catch (e) {
+            console.error(e)
+        }
     }
-    registerHandler = () => {
+    registerHandler = async () => {
+        const authData = {
+            email: this.state.formControls.email.value,
+            password: this.state.formControls.password.value,
+            returnSecureToken: true
+        }
 
+        try {
+            const response = await axios.post(`https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyAevsYkAm2KDeefKTe9Jw20l9tCypNiWIU`, authData)
+
+            console.log(response.data)
+        } catch (e) {
+            console.error(e)
+        }
     }
 
     submitHandler = event => {
