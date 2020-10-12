cd65ba29d4031cf90f4f6eae6e74c9b0c59c8329
matviiko <matviy.kopchak@gmail.com>
2020-10-12 18:53:03 +0300

    add  Redux Storage at auth containers

--- src/App.js
+++ src/App.js
@@ -1,26 +1,62 @@
 import React, {Component} from 'react';
 import Layout from "./hoc/Layout/Layout";
-import {Route, Switch} from "react-router-dom"
+import {Route, Switch, Redirect, withRouter} from "react-router-dom"
 import Quiz from "./containers/Quiz/Quiz";
 import QuizList from "./containers/QuizList/QuizList";
 import QuizCreator from "./containers/QuizCreator/QuizCreator";
 import Auth from "./containers/Auth/Auth";
+import {connect} from "react-redux";
+import Logout from "./components/Logout/Logout";
+import {autoLogin} from "./store/actions/auth";
 
 class App extends Component {
 
+    componentDidMount() {
+        this.props.autoLogin()
+    }
 
-  render() {
-    return (
-        <Layout>
+    render() {
+
+      let routes = (
+          <Switch>
+              <Route path="/auth" component={Auth}/>
+              <Route path="/quiz-creator" component={QuizCreator}/>
+              <Route path="/quiz/:id" component={Quiz}/>
+              <Route path="/" exact component={QuizList}/>
+              <Redirect to={"/"}/>
+          </Switch>
+      )
+
+      if (this.props.isAuthentication) {
+        routes = (
             <Switch>
-                <Route path="/auth" component={Auth}/>
                 <Route path="/quiz-creator" component={QuizCreator}/>
                 <Route path="/quiz/:id" component={Quiz}/>
-                <Route path="/" component={QuizList}/>
-            </Switch>
-        </Layout>
-    );
+                <Route path="/logout" component={Logout}/>
+                <Route path="/" exact component={QuizList}/>
+                <Redirect to={"/"}/>
+v            </Switch>
+        )
+      }
+
+      return (
+          <Layout>
+              { routes }
+          </Layout>
+     );
   }
 }
 
-  export default App;
+function mapStateToProps(state) {
+    return{
+        isAuthentication: !!state.auth.token
+    }
+}
+
+function mapDispatchToProps(dispatch) {
+    return {
+        autoLogin: () => dispatch(autoLogin())
+    }
+}
+
+export default withRouter(connect(mapStateToProps, mapDispatchToProps)(App));
